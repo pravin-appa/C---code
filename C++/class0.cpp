857cf9db8086b49aa307608687e3ec68ee1c6546
PRAVIN MOHAN SURYAWANSHI <62396023+pravin-appa@users.noreply.github.com>
2020-08-14 22:29:28 +0530

    c++
    class and object intro

---
+++ C++/class0.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+ class Appa{
+    public:
+       void yo(){
+           cout << "I am the greatest" << endl;
+       }
+
+   };
+
+int main()
+{
+    Appa as;
+    as.yo();
+    return 0;
+}
+
