1ca30601bcad739395a5385f7bae0cb249635e4e
PRAVIN MOHAN SURYAWANSHI <62396023+pravin-appa@users.noreply.github.com>
2020-09-22 19:24:58 +0530

    C++ coding
    Filestream handling and getline instruction understanding

---
+++ C++/C++ codes needs to be execute as in a project/getline.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string.h>
+
+
+using namespace std;
+
+
+int main()
+{
+       string name; // variable declaration.
+       std::cout << "Enter your name :" << std::endl;
+       getline(cin,name); // implementing a getline() function
+       cout<<"\nHello "<<name;
+return 0;}
