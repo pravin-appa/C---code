b896bf2548f86111a1ea131af87f09ca19e9b405
PRAVIN MOHAN SURYAWANSHI <62396023+pravin-appa@users.noreply.github.com>
2020-10-15 20:56:39 +0530

    C++ coding
    Namespace naming concept

---
+++ C++/C++ codes needs to be execute as in a project/namespace.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+namespace addition
+{
+    int a=5;
+    int b=5;
+    int add()
+    {
+        return(a+b);
+    }
+}
+
+int main() {
+int result;
+result=addition::add();
+cout<<result;
+return 0;
+}
